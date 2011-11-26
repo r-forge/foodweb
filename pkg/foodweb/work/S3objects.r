a9ea6821edd15f71e35c5a123bfd7b4d84905049
flo_schneider (Florian Schneider) <f_schneider@bio.tu-darmstadt.de>
2011-11-26 20:48:55 +0000

    updated S3objects.r  following Karlines comments (see e-mail from 26.11.2011)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/foodweb@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/foodweb/work/S3objects.r
+++ pkg/foodweb/work/S3objects.r
@@ -1,33 +1,45 @@
 ####################################################
 #   foodwebs package - the foodweb S3 object classes
 #
-#   last edit: 03.10.2011
+#   last edit: 26.11.2011
 #   
-#	implemented: classes reference, metadata, nodes, linklist, foodweb
+#	implemented: classes metadata, nodes, linklist, foodweb
 #			generic function as.linklist and as.foodweb
 #			
 #
 #	todo: 	as.linklist.matrix should convert matrix with numerical entries to a linklist with 3 columns
 #			generic function as.webmatrix and methods to create valid webmatrix objects
-#			a print method for foodweb objects
+#			print methods for "foodweb" and "metadata" objects
 #
 #
 #####################################################
-	
+
 
 	
 as.linklist <- function(x, ...) UseMethod("as.linklist")   #generic function
 
+
+# S3 method generates new linklist object from two numeric vectors(plus optional vectors for link information)  
+as.linklist.numeric <- function(pred, prey, ...) {
+	if(! is.vector(pred)) stop("could not create 'linklist' object: 'pred' has to be of class 'vector' !")
+	if(! is.vector(prey)) stop("could not create 'linklist' object: 'prey' has to be of class 'vector' !")
+	if(length(pred) != length(prey)) stop("could not create 'linklist' object: 'pred' and 'prey' must be of same length !")
+	
+	as.linklist(data.frame(pred, prey, ...))	
+}
+
+
+
 # S3 method to convert objects of class 'data.frame' into 'linklist'
-as.linklist.data.frame <- function(x, pred.first = TRUE) {		#converts data.frame to linklist object, checks for valid data
+as.linklist.data.frame <- function(x, pred.first = TRUE, ...) {		#converts data.frame to linklist object, checks for valid data
 		cols <- length(x[1,])	#number of columns
 		
 		if(pred.first == TRUE) names(x)[1:2] <- c("pred", "prey")  #if object has predators as first column (default)
 		else names(x)[1:2] <- c("prey", "pred") 				#if object has prey as first column
 
 		# check if data.frame fits conditions
-		if(class(x$pred) != "integer") stop("no valid data.frame to create linklist object: 'pred' contains non-integer")
-		else if(class(x$prey) != "integer") stop("no valid data.frame to create linklist object: 'prey' contains non-integer ")
+		if( FALSE %in% (round(x$pred, digits= 0) == x$pred) ) stop("no valid data.frame to create linklist object: 'pred' contains non-integer")
+		else if( FALSE %in% (round(x$prey, digits= 0) == x$prey) ) stop("no valid data.frame to create linklist object: 'prey' contains non-integer ")
 		# here, other conditions for this class can be defined
 		# e.g. the foodweb needs to be cohesive (all species are indirectly linked to each other)
 		
@@ -42,7 +54,7 @@ as.linklist.data.frame <- function(x, pred.first = TRUE) {		#converts data.frame
 	
 # S3 method converts objects of class 'matrix' to 'linklist', checks for validity of matrix 
 # creates binary linklist only!!!
-as.linklist.matrix <- function(x, pred.rows = TRUE) {
+as.linklist.matrix <- function(x, pred.rows = TRUE, ...) {
 		# check if matrix fits conditions
 		if( length(x[,1]) != length(x[1,])) stop("no valid matrix to create linklist: number of columns and number of rows differ!")
 		# here, other conditions for this class can be defined
@@ -71,77 +83,37 @@ as.linklist.webmatrix <- function(x) {
 as.linklist.foodweb <- function(x) x$linklist
 
 
-# function generates new linklist object from two (or more) vectors 
-linklist <- function(pred, prey, ...) {
-	if(! "vector" %in% class(pred)) stop("could not create 'linklist' object: 'pred' has to be of class 'vector' !")
-	if(! "vector" %in% class(prey)) stop("could not create 'linklist' object: 'prey' has to be of class 'vector' !")
-	if(length(pred) != length(prey)) stop("could not create 'linklist' object: 'pred' and 'prey' must be of same length !")
-	
-	as.linklist(data.frame(pred, prey, ...))	
-}
 
 
-# function creates list object of class 'reference'. mandatory parameters are title, author, year. default bibtype is 'article'. 
-reference <- function(
-					title, 
-					author, 
-					year, 
-					journal = NULL, 
-					volume = NULL, 
-					pages = NULL, 
-					publisher = NULL, 
-					issue = NULL, 
-					bibtype = "article") {
-					
-				if(bibtype != "article" && bibtype != "book") stop(cat(bibtype, " is no valid entry for parameter 'bibtype'!", sep = ""))  
-				
-				object <- list(
-					title = title, 
-					author = author, 
-					year = year, 
-					journal = journal,
-					volume = volume, 	
-					pages = pages,
-					publisher = publisher, 
-					issue = issue,   
-					bibtype = bibtype)
-				class(object) <- c("reference")
-				return(object)
-	}
 
-# print method for reference-objects
-print.reference <- function(object) {
-		if(object$bibtype == "article") out <- cat(object$author, " (", object$year, ") ", object$title, ", ", object$journal, ", ", object$volume, ":", object$pages, "\n", sep = "") 
-		else if(object$bibtype == "book") out <-  cat(object$author, " (", object$year, ") ", object$title, ", ", object$publisher, ", ", object$issue, "\n", sep = "") 
-		#else if(object@bibtype == "bookchapter") out <- ...
-	
-		else stop(cat(object$bibtype, " is no valid entry for parameter 'bibtype'!", sep = ""))
-		return(out)
-}
 
 # creates metadata object, only parameter 'name' is mandatory. 
 metadata <- function(
 		name, 
-		type = NULL, 
+		type = NULL, 							# empirical webs can be "terrestrial", "marine", "grassland" and generated webs can be "niche", "nestedhierarchy", "adbm" etc.
 		source = NULL, 							# generator functions should create metadata with source = 'generator' and type = 'model.type'
 		author = NULL, 							
 		year = NULL, 
 		country = NULL, 
-		reference = NULL, 					  	#reference object
+		reference = NULL, 					  	#object of class bibentry
 		lat = NULL, lon = NULL, alt = NULL,   	#latitude, longitude, altitude
 		description = NULL) {				  	#contains detailed information on the empirical web
 	
 		#checking conditions for metadata object
-		if(source != "empirical" && source != "model") stop(cat("could not create metadata object: '", source ,"' is no valid value for parameter 'source'!"))
-		if(! "NULL" %in% class(reference) && ! "reference" %in% class(reference) ) stop(cat("could not create metadata object: '", reference , "' is no valid object for 'reference'! ") )
+		if(class(source) != "NULL")  if(! source %in% c("empirical", "model")) stop(cat("could not create metadata object: '", source ,"' is no valid value for parameter 'source'!/n", sep = ""))
+		if(! class(reference) %in% c("NULL", "bibentry") ) stop(cat("could not create metadata object: please provide an object of class 'bibentry' for 'reference'!/n Type '?bibentry' for further information on that object class!/n", sep = "") )
 		# here, other conditions for this class can be defined
 		
-		# if reference is provided, author and year could be extracted automatically
+		# if a reference is provided, author and year could be extracted automatically
 		
 		object <- list(name = name, type = type , source = source , author = author, year = year, country = country, reference = reference, lat = lat, lon = lon, alt = alt, description = description)
 		class(object) <- c("metadata", "list")
 		return(object)
 }
+
+#print.metadata <- function()
+
+
 
 #function creates object of class 'nodes'
 nodes <- function(x, ...) {
